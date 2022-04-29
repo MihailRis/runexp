f38b72fe022588e622679b4fb9a956b194eaec18
MihailRis <mexaruc@gmail.com>
2022-04-29 23:28:39 +0300

    More docs

--- src/mihailris/runexp/ExpMaths.java
+++ src/mihailris/runexp/ExpMaths.java
@@ -2,13 +2,21 @@ package mihailris.runexp;
 
 import java.util.Random;
 
+/**
+ * Class for implementation of special RunExp built-in functions
+ */
 public class ExpMaths {
     public static final Random random = new Random();
 
     public static float smoother(float a){
         return a * a * a * (a * (a * 6 - 15) + 10);
     }
-    public static float rand(float a){
+
+    /**
+     * @param a used to make function non-constant
+     * @return pseudorandom value in range [0.0, 1.0] not depending on parameter
+     */
+    public static float rand(@SuppressWarnings("unused") float a){
         return random.nextFloat();
     }
 }
